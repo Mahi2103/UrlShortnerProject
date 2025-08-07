58f4f1e2b08135ddb6ee064d2658d9459ae363fb
Mahi2103 <mjcode2103@gmail.com>
2025-08-07 18:42:10 +0530

    models

---
+++ UrlShortner.Server/Model/AccessLog.cs
@@ -0,0 +1,7 @@
+public class AccessLog
+{
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    public string IpAddress { get; set; }
+    public string Device { get; set; }
+    public string Browser { get; set; }
+}
---
+++ UrlShortner.Server/Model/UrlMapping.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class UrlMapping
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string OriginalUrl { get; set; }
+
+    public string ShortCode { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? ExpiresAt { get; set; }
+
+    public int Clicks { get; set; } = 0;
+
+    public List<AccessLog> AccessLogs { get; set; } = new List<AccessLog>();
+
+    public bool IsPasswordProtected { get; set; } = false;
+
+    public string? PasswordHash { get; set; }
+
+    public string? QrCodeUrl { get; set; }
+}
+  
